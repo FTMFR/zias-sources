2a1244ac8b214083eded7efa82ec762af72f5727
FTMFR <fatemefarajzade018@gmail.com>
2024-10-25 22:28:34 +0330

    add (SeeMoreBtn): add SeeMoreBtn component
    -Implement SeeMoreBtn component for see more blog/products and go to their pages

---
+++ src/components/Btns/SeeMoreBtn.jsx
@@ -0,0 +1,6 @@
+const SeeMoreBtn = () => {
+    // add onclick event for if click on this btn go to /product URL
+  return <button className="btn-seeMore">مشاهده بیشتر</button>;
+};
+
+export default SeeMoreBtn;
