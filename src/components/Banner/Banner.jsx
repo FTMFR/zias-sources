57500ccfa8a1c71c934ecf57f93f317624da54db
FTMFR <fatemefarajzade018@gmail.com>
2024-10-25 13:38:43 +0330

    chor (Banner): create Banner structure for add BAnner in future

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,9 @@
+import "../../styles/globals.css";
+
+const Banner = () => {
+  return (
+    <h1 className="w-full h-12 banner flex items-center justify-center font-base font-bold">بنر اینجا قرار خواهد گرفت</h1>
+  );
+};
+
+export default Banner;
