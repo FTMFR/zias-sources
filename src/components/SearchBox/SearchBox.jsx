3be790d180ec101e12c8ab238c15cd58df4221c8
FTMFR <fatemefarajzade018@gmail.com>
2024-10-25 14:56:14 +0330

    feat (SearchBox): add comment for future

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -2,12 +2,13 @@ import Image from "next/image";
 
 const SearchBox = () => {
   return (
+    // add functionality that if click on this section, user can search anything in site
     <div className="flex w-10/12 gap-2 mx-auto my-3 searchBox p-2 rounded-md">
       <Image
         height="24"
         width="24"
         src="/images/Search 1.svg"
-        alt="search icon" 
+        alt="search icon"
       />
       <div className="flex gap-1 searchBoxText">
         جستجو در
