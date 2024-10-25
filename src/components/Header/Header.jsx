21d1d3bb81144a9ba254721708e2fe5a241a9214
FTMFR <fatemefarajzade018@gmail.com>
2024-10-25 14:54:46 +0330

    refactor (Header): edit HEader component based on rtl direction that add later

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,17 +2,19 @@ import Image from "next/image";
 
 const Header = () => {
   return (
-    <div className="flex w-10/12 h-12 items-center justify-center mx-auto border-2 border-black rounded-md mt-1">
-      <div className="flex flex-1 items-center gap-1">
-        {/* add onclick event for show bag shop overview */}
-        <div className="relative">
-          <span className="absolute -right-1 top-3 flex justify-center items-center bg-brand-primary rounded-full h-5 w-5 text-white font-bold text-14B">
-            5
-          </span>
-          <Image width="30" height="30" src="/images/bag.png" alt="bag shop" />
-        </div>
-        {/* add onclick event for show profile overiew */}
-        <Image width="30" height="30" src="/images/profile.png" alt="profile" />
+    <div className="flex w-12/12 h-12 items-center justify-center mx-auto border-2 border-black rounded-md mt-1">
+      <div className="flex justify-center items-center gap-1">
+        {/* add onclick event for show sidebar menu */}
+        <Image
+          width="45"
+          height="45"
+          src="/images/line-3-light.png"
+          alt="menu"
+        />
+        {/* should be a link with href:homePage */}
+        <h1 className="zias">ZIAS</h1>
+      </div>
+      <div className="flex flex-1 items-center justify-end gap-1">
         {/* add onclick event for show notifications overview */}
         <div className="relative">
           <span className="absolute -right-1 top-3 flex justify-center items-center bg-brand-primary rounded-full h-5 w-5 text-white font-bold text-14B">
@@ -25,18 +27,16 @@ const Header = () => {
             alt="notifications"
           />
         </div>
-      </div>
-      <div className="flex justify-center items-center gap-1">
-        {/* should be a link with href:homePage */}
-        <h1 className="zias">ZIAS</h1>
-
-        {/* add onclick event for show sidebar menu */}
-        <Image
-          width="45"
-          height="45"
-          src="/images/line-3-light.png"
-          alt="menu"
-        />
+         {/* add onclick event for show profile overiew */}
+         <Image width="30" height="30" src="/images/profile.png" alt="profile" />
+        {/* add onclick event for show bag shop overview */}
+        <div className="relative">
+          <span className="absolute -right-1 top-3 flex justify-center items-center bg-brand-primary rounded-full h-5 w-5 text-white font-bold text-14B">
+            5
+          </span>
+          <Image width="30" height="30" src="/images/bag.png" alt="bag shop" />
+        </div>
+       
       </div>
     </div>
   );
