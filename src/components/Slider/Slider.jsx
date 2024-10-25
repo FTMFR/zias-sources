8ce5013514441eb8943fd81f827e574f913033e8
FTMFR <fatemefarajzade018@gmail.com>
2024-10-25 15:30:24 +0330

    add (Slider): add Slider for paginate sliders

---
+++ src/components/Slider/Slider.jsx
@@ -0,0 +1,16 @@
+const Slider = () => {
+  return (
+    <div className="flex gap-1.5">
+    
+      <div className="circle"></div>
+      <div className="circle"></div>
+      <div className="circle"></div>
+      <div className="circle active"></div>
+      <div className="circle"></div>
+      <div className="circle"></div>
+      <div className="circle"></div>
+    </div>
+  );
+};
+
+export default Slider;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,7 @@
 import Banner from "../components/Banner/Banner";
 import Header from "../components/Header/Header";
 import SearchBox from "../components/SearchBox/SearchBox";
+import SliderImg from "../components/Slider/SliderImg";
 
 function HomePage() {
   return (
@@ -9,6 +10,7 @@ function HomePage() {
       <div className="container">
         <Header />
         <SearchBox />
+        <SliderImg />
       </div>
     </>
   );
