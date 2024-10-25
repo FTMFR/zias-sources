3df6c18800d03870a1ef9b6b8ec270b00b0edf4c
FTMFR <fatemefarajzade018@gmail.com>
2024-10-25 20:01:05 +0330

    refactor (Banner): edit Banner text

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -2,7 +2,9 @@ import "../../styles/globals.css";
 
 const Banner = () => {
   return (
-    <h1 className="w-full h-12 banner flex items-center justify-center font-base font-bold">بنر اینجا قرار خواهد گرفت</h1>
+    <h1 className="w-full h-12 banner flex items-center justify-center font-base font-bold">
+      ...Coming Soon
+    </h1>
   );
 };
 
