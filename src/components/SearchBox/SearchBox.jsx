bd3fdb701d69990b236ecbb6bfc7cac164e0709f
FTMFR <fatemefarajzade018@gmail.com>
2024-10-25 14:54:11 +0330

    chor (SearchBox): create SearchBox component

---
+++ src/components/SearchBox/SearchBox.jsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+
+const SearchBox = () => {
+  return (
+    <div className="flex w-10/12 gap-2 mx-auto my-3 searchBox p-2 rounded-md">
+      <Image
+        height="24"
+        width="24"
+        src="/images/Search 1.svg"
+        alt="search icon" 
+      />
+      <div className="flex gap-1 searchBoxText">
+        جستجو در
+        <div className="searchBoxZias font-bold"> زی‌آث</div>
+      </div>
+    </div>
+  );
+};
+
+export default SearchBox;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,15 @@
 import Banner from "../components/Banner/Banner";
 import Header from "../components/Header/Header";
+import SearchBox from "../components/SearchBox/SearchBox";
 
 function HomePage() {
   return (
     <>
       <Banner />
-      <Header />
+      <div className="container">
+        <Header />
+        <SearchBox />
+      </div>
     </>
   );
 }
