20e18a9fac4bc84a88c895ec1addd5a452079325
FTMFR <fatemefarajzade018@gmail.com>
2024-10-28 18:26:35 +0330

    feat (Header): change Header
    -Remove notif and profile button
    -Add Login/Signup button for user's login

--- store/src/components/Header/Header.jsx
+++ store/src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
-const Header = () => {
+const Header = ({ toggleModal }) => {
   return (
     <div className="flex w-12/12 h-12 items-center justify-center mx-auto border-2 border-black rounded-md mt-1">
       <div className="flex justify-center items-center gap-1">
@@ -16,7 +17,8 @@ const Header = () => {
       </div>
       <div className="flex flex-1 items-center justify-end gap-1">
         {/* add onclick event for show notifications overview */}
-        <div className="relative">
+        {/* if user doesn't login yet in site, should show them login/signup button */}
+        {/* <div className="relative">
           <span className="absolute -right-1 top-3 flex justify-center items-center bg-brand-primary rounded-full h-5 w-5 text-white font-bold text-14B">
             5
           </span>
@@ -27,9 +29,18 @@ const Header = () => {
             alt="notifications"
             style={{ width: "auto", height: "auto" }}
           />
-        </div>
+        </div> */}
         {/* add onclick event for show profile overiew */}
-        <Image width="30" height="30" src="/images/profile.png" alt="profile" />
+        {/* <Image width="30" height="30" src="/images/profile.png" alt="profile" /> */}
+        <div className="flex flex-row text-[10px] font-bold p-1 justify-center items-center gap-1 login-button">
+          <Image
+            width={20}
+            height={20}
+            src="/images/login.svg"
+            alt="login icon"
+          />
+          <div onClick={toggleModal}>ورود | ثبت نام</div>
+        </div>
         {/* add onclick event for show bag shop overview */}
         <div className="relative">
           <span className="absolute -right-1 top-3 flex justify-center items-center bg-brand-primary rounded-full h-5 w-5 text-white font-bold text-14B">
