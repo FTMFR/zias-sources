2429c2df8a2170e457cff03db26ceec607ca86d9
FTMFR <fatemefarajzade018@gmail.com>
2024-10-28 18:32:11 +0330

    add (index): add login/signup functionality in this oage

--- store/src/pages/index.jsx
+++ store/src/pages/index.jsx
@@ -1,23 +1,51 @@
+import { useState } from "react";
 import Banner from "../components/Banner/Banner";
 import Blogs from "../components/Blogs/Blogs";
 import Footer from "../components/Footer/Footer";
 import Header from "../components/Header/Header";
+import Signup from "../components/LoginModal/signup";
 // import LoginModal from "../components/LoginModal/LoginModal";
 // import Signup from "../components/LoginModal/signup";
 import ProductsCart from "../components/Product/ProductsCart";
 import SearchBox from "../components/SearchBox/SearchBox";
 import SliderImg from "../components/Slider/SliderImg";
 import Sliders from "../components/Slider/Sliders";
+import LoginModal from "../components/LoginModal/LoginModal";
 
-function HomePage() {
+const HomePage = () => {
+  const [isModalVisible, setModalVisible] = useState(false);
+  const [isNextStapVisible, setNextStapVisible] = useState(false);
+
+  const toggleModal = () => {
+    setModalVisible((prev) => !prev);
+  };
+
+  const toggleNextStepModal = () => {
+    setNextStapVisible((prev) => !prev);
+  };
+
+  const nextStepLogin = () => {
+    setNextStapVisible(true);
+  };
+
   return (
     <>
       {/* ‌banner goes here */}
       <Banner />
       {/* add container class for create a container with width 80% */}
+      {isModalVisible && (
+        <Signup
+          toggleModal={toggleModal}
+          onClose={toggleModal}
+          nextStepLogin={nextStepLogin}
+        />
+      )}
+      {isNextStapVisible && (
+        <LoginModal toggleModal={toggleNextStepModal} onClose={toggleNextStepModal} />
+      )}
       <div className="container">
         {/* add header component */}
-        <Header />
+        <Header toggleModal={toggleModal} />
         {/* add search box component */}
         <SearchBox />
         {/* add slider box img component */}
@@ -35,6 +63,6 @@ function HomePage() {
       <Footer />
     </>
   );
-}
+};
 
 export default HomePage;
